11258aa52124f33cb0713b86a9fc01764114d304
ilyaleonovich28-fr <ilyaleonovich28@gmail.com>
2026-04-23 19:03:51 +0200

    homework_3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,7 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(firstNumber, secondNumber) {
+  return (firstNumber + secondNumber) / 2;
+}
+let result = calculateAverage(8, 10);
+console.log(result);
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,13 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(firstNumber, secondNumber, thirdNumber) {
+  if (firstNumber > secondNumber && firstNumber > thirdNumber) {
+    return firstNumber;
+  } else if (secondNumber > firstNumber && secondNumber > thirdNumber) {
+    return secondNumber;
+  } else {
+    return thirdNumber;
+  }
+}
+let result = findLargest(44, 55, 99);
+console.log(result);
